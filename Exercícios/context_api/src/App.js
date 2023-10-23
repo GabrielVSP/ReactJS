89d7bc2efab98e367d3292bdf16533d79bec4419
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2023-10-22 22:16:49 -0300

    exercício de react query

--- Exercícios/context_api/src/App.js
+++ Exercícios/context_api/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/home';
 import Contact from './pages/contact';
 import Menu from './pages/menu';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState, createContext } from 'react';
 
 export const AppContext = createContext()
@@ -11,20 +13,31 @@ function App() {
 
   const [username, setUsername] = useState('Gabriel')
 
+  const queryClient = new QueryClient({defaultOptions: {
+
+    queries: {
+      refetchOnWindowFocus: false,
+    }
+
+  } })
+
   return (
 
     <div className="App">
       
-      <AppContext.Provider value={{username, setUsername}}>
-        <Router>
-          <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='/contact' element={<Contact />} />
-            <Route exact path='/menu' element={<Menu />} />
-            <Route exact path='*' element={<h1>Page not found</h1>} />
-          </Routes>
-        </Router>
-      </AppContext.Provider>
+      <QueryClientProvider client={queryClient}>
+        <AppContext.Provider value={{username, setUsername}}>
+          <Router>
+              <Routes>
+                <Route exact path='/' element={<Home />} />
+                <Route exact path='/contact' element={<Contact />} />
+                <Route exact path='/menu' element={<Menu />} />
+                <Route exact path='*' element={<h1>Page not found</h1>} />
+        
+              </Routes>
+          </Router>
+        </AppContext.Provider>
+      </QueryClientProvider>
 
     </div>
 
--- Exercícios/context_api/src/pages/home.js
+++ Exercícios/context_api/src/pages/home.js
@@ -1,11 +1,30 @@
 import { useContext } from "react"
 import { Link } from "react-router-dom"
+import { Axios } from 'axios'
 
 import {AppContext} from '../App'
+import { useQuery } from "@tanstack/react-query"
 
 export default function Home() {
 
     const {username} = useContext(AppContext)
+    /*const { data } = useQuery(['cat'], () => {
+
+       return Axios.get('https://https://catfact.ninja/').then((res) => res.data)
+
+    })*/
+
+    const { isPending, error, data, refetch } = useQuery({
+        queryKey: ['catFact'],
+        queryFn: () =>
+          fetch('https://catfact.ninja/fact').then(
+            (res) => res.json(),
+          ),
+      })
+
+    if (isPending) return 'Loading...'
+
+    if (error) return 'An error has occurred: ' + error.message
 
     return (
 
@@ -18,6 +37,9 @@ export default function Home() {
             <Link to={'/menu'}>Menu</Link>
 
             <h2>Hello, {username}!</h2>
+
+            <button onClick={refetch}>Generate</button>
+            <p>Cat fact: {data?.fact}</p>
 
         </div>
 
